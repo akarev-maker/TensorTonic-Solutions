63d3ba4de55c2904902f26d977bc5678c4685f74
Andrew Karev <akarev@umass.edu>
2026-09-19 16:40:19 -0400

    Solve mean-median-mode

--- mean-median-mode/mean-median-mode.py
+++ mean-median-mode/mean-median-mode.py
@@ -6,19 +6,14 @@ def mean_median_mode(x: list) -> dict:
     Returns a dictionary with mean, median, and mode.
     """
     # Write code here
-    x.sort()
-    n = len(x)
-    count = Counter(x)
-    sum = 0
-    mode = x[0]
-    for num in x:
-        sum += num
-        if count[num] > count[mode]:
-            mode = num
-        if num < mode and count[num] == count[mode]:
-            mode = num
-    mean = sum / n
-    median = (x[n // 2] + x[(n-1) // 2]) / 2
-    return {"mean" : mean, "median" : median, "mode" : float(mode)}
+    counts = Counter(x)
+    x = np.asarray(x, dtype=float)
+    highest_frequency = max(counts.values())
+    mode = min(value for value, count in counts.items() if count == highest_frequency)
+    return {
+        "mean": float(np.mean(x)),
+        "median": float(np.median(x)),
+        "mode": float(mode),
+    }
         
         
