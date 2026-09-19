4d6d1b1b6b0c082b70e63e5a2de5e2cad997bd60
Andrew Karev <akarev@umass.edu>
2026-09-19 13:09:33 -0400

    Solve dot-product

---
+++ dot-product/dot-product.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def dot_product(x: list, y: list) -> float:
+    """
+    Returns the dot product as a float.
+    """
+    # Write code here
+    sum = 0.0
+    for i in range(len(x)):
+        sum += x[i] * y[i]
+    return sum
