7741f13f84bcd4807a91ed8fa5cea16a32163005
Andrew Karev <akarev@umass.edu>
2026-09-19 14:08:53 -0400

    Solve matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -5,6 +5,10 @@ def matrix_transpose(A: list) -> np.ndarray:
     Returns the transposed matrix as a NumPy array.
     """
     # Write code here
-    matrix = np.array(A)
-    return matrix.T
-        
+    n = len(A)
+    m = len(A[0])
+    transposed = np.zeros((m, n), dtype=type(A[0][0]))
+    for i in range(n):
+        for j in range(m):
+            transposed[j][i] = A[i][j]
+    return transposed
