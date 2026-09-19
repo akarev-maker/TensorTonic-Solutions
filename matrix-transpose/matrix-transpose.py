a29c250cf4ab1b4c9f0261c06a860e144f88fcfa
Andrew Karev <akarev@umass.edu>
2026-09-19 13:52:24 -0400

    Solve matrix-transpose

---
+++ matrix-transpose/matrix-transpose.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def matrix_transpose(A: list) -> np.ndarray:
+    """
+    Returns the transposed matrix as a NumPy array.
+    """
+    # Write code here
+    matrix = np.array(A)
+    return matrix.T
+        
